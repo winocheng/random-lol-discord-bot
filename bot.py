05e49943a4e7cc50dd89ce4868142aaf5d1e369a
winocheng <wino9417@gmail.com>
2024-12-30 23:40:42 +0800

    added reroll button

--- bot.py
+++ bot.py
@@ -8,11 +8,13 @@ BOT_TOKEN = os.getenv('TOKEN', "Token Doesn't Exist!")
 POSITIONS = ['top', 'jungle', 'mid', 'adc', 'support']
 bot = discord.Bot()
 
-# we need to limit the guilds for testing purposes
-# so other users wouldn't see the command that we're testing
+# reroll button view
+class RerollButtonView(discord.ui.View):
+    @discord.ui.button(label="Reroll", style=discord.ButtonStyle.secondary, emoji="🔄") # Create a button with the label "reroll"
+    async def button_callback(self, button, interaction):
+        await interaction.response.edit_message(content=rollComp()) # Send a message when the button is clicked
 
-@bot.command(description="Generate a random league team composition") # this decorator makes a slash command
-async def randomcomp(ctx): # a slash command will be created with the name "ping"
+def rollComp():
     champions = dict()
     while not champions or len(set([values[0] for _, values in champions.items()])) != 5:
         for pos in POSITIONS:
@@ -25,6 +27,10 @@ async def randomcomp(ctx): # a slash command will be created with the name "ping
     res += f"Bot: {champions['adc'][0]} {champions['adc'][1]}\n"
     res += f"Support: {champions['support'][0]} {champions['support'][1]}\n"
 
-    await ctx.respond(res)
+    return res
+
+@bot.command(description="Generate a random league team composition") # this decorator makes a slash command
+async def randomcomp(ctx): # a slash command will be created with the name "randomcomp"
+    await ctx.respond(rollComp(), view=RerollButtonView())
 
 bot.run(BOT_TOKEN)
